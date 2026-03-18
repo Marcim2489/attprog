8d5ec8afb1133dd10b700ce179390032d9b2bd2c
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-03-18 15:25:42 -0300

    primeiro commit

---
+++ IndiceMassaCorporal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace IndiceMassaCorporal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso;
+            float altura;
+            
+            Console.WriteLine("Use vírgula para representar os decimais.");
+            Console.WriteLine();
+
+            Console.WriteLine("Insira seu peso.");
+            while (float.TryParse(Console.ReadLine(), out peso) == false)
+            {
+                Console.WriteLine("Input inválido! Tente novamente.");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Insira sua altura.");
+            while (float.TryParse(Console.ReadLine(), out altura) == false)
+            {
+                Console.WriteLine("Input inválido! Tente novamente.");
+            }
+            Console.WriteLine();
+
+            float indiceMassaCorporal= peso / (altura * altura);
+            Console.WriteLine("Seu índice de massa corporal é: "+indiceMassaCorporal);
+
+            string classificacao="";
+            if (indiceMassaCorporal <= 16) classificacao = "Magreza grau III";
+            else if (indiceMassaCorporal <= 16.9f) classificacao = "Magreza grau II";
+            else if (indiceMassaCorporal <= 18.4f) classificacao = "Magreza grau I";
+            else if (indiceMassaCorporal <= 24.9f) classificacao = "Adequado";
+            else if (indiceMassaCorporal <= 29.9f) classificacao = "Pré-obeso";
+            else if (indiceMassaCorporal <= 34.9f) classificacao = "Obesidade grau I";
+            else if (indiceMassaCorporal <= 39.9f) classificacao = "Obesidade grau II";
+            else classificacao = "Obesidade grau III";
+
+            Console.WriteLine("Sua classificação é: "+classificacao);
+        }
+    }
+}
---
+++ MiniCalculadora/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MiniCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcaoEscolhida=0;
+            do
+            {
+                Console.WriteLine("Vamos fazer um cálculo! Represente as decimais com vírgula.");
+                Console.WriteLine();
+
+                float primeiroNumero;
+                float segundoNumero;
+
+                Console.WriteLine("Insira o primeiro número.");
+                while (float.TryParse(Console.ReadLine(), out primeiroNumero)==false)
+                {
+                    Console.WriteLine("Input inválido! Digite o primeiro número novamente.");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Insira o segundo número.");
+                while (float.TryParse(Console.ReadLine(), out segundoNumero) == false)
+                {
+                    Console.WriteLine("Input inválido!  Digite o segundo número novamente.");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Agora selecione a operação");
+                Console.WriteLine("1 - Soma");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Sair");
+                while (int.TryParse(Console.ReadLine(), out opcaoEscolhida) == false || opcaoEscolhida<1 || opcaoEscolhida >5)
+                {
+                    Console.WriteLine("Input inválido!  Digite a operação desejada novamente.");
+                }
+                Console.WriteLine();
+                switch (opcaoEscolhida) 
+                {
+                    case 1:
+                        Console.WriteLine("O resultado da soma é " + (primeiroNumero + segundoNumero) + "!");
+                        break;
+                    case 2:
+                        Console.WriteLine("O resultado da subtração é " + (primeiroNumero - segundoNumero) + "!");
+                        break;
+                    case 3:
+                        Console.WriteLine("O resultado da multiplicação é " + (primeiroNumero * segundoNumero) + "!");
+                        break;
+                    case 4:
+                        Console.WriteLine("O resultado da divisão é " + (primeiroNumero/segundoNumero) + "!");
+                        break;
+                    case 5:
+                        Console.WriteLine("Adeus!");
+                        break;
+                    default:
+                        Console.WriteLine("Algo deu errado...");
+                        break;
+                }
+                
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (opcaoEscolhida != 5);
+        }
+    }
+}
