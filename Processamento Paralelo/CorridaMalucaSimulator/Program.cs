7f5042a51b9078097eb77ede8c8fbb94137d40cf
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-04-11 11:10:08 -0300

    exercícios de processamento paralelo

---
+++ Processamento Paralelo/CorridaMalucaSimulator/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CorridaMalucaSimulator
+{
+    internal class Program
+    {
+        static List<Carro> classificacaoFinal = new List<Carro>();
+        static object lockObject = new object();
+        static Random random = new Random();
+        static int numeroTotalDeVoltas = 10;
+        static List<Thread> threads = new List<Thread>();
+        static List<Carro> competidores = new List<Carro>()
+        {
+            new Carro(200, "Ferrari", 200),
+            new Carro(300, "McLaren", 80),
+            new Carro(250, "Mercedes", 130)
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de corredores:");
+            Console.WriteLine();
+            foreach (Carro carro in competidores)
+            {
+                Thread t = new Thread(() =>
+                {
+                    Correr(carro);
+                });
+                threads.Add(t);
+                Console.WriteLine(carro.Nome);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para iniciar a corrida!");
+            Console.ReadKey();
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }    
+
+            Console.WriteLine();
+            for (int i = 0; i < classificacaoFinal.Count; i++)
+            {
+                Console.WriteLine($"{i+1}° - {classificacaoFinal[i].Nome}");
+            }
+        }
+
+        static void Correr(Carro carro)
+        {
+            int tempoDaVoltaAntesDoPitStop = 0;
+            float chanceDePitStop = 50f;
+            while (carro.VoltasPercorridas < numeroTotalDeVoltas)
+            {
+                if(carro.VoltasPercorridas >= 3 && random.Next(0, 101) < chanceDePitStop)
+                {
+                    tempoDaVoltaAntesDoPitStop = random.Next(1, carro.TempoDeCadaVolta);
+                    Console.WriteLine($"{carro.Nome} parou no pit stop!");
+                    chanceDePitStop *= 0.1f;
+                    Thread.Sleep(carro.TempoDePitStop);
+                }
+                Thread.Sleep(carro.TempoDeCadaVolta-tempoDaVoltaAntesDoPitStop);
+                tempoDaVoltaAntesDoPitStop = 0;
+                carro.CompletarVolta();
+            }
+            lock (lockObject) 
+            {
+                classificacaoFinal.Add(carro);
+            }
+        }
+    }
+
+    class Carro
+    {
+        public int TempoDeCadaVolta { get; private set; }
+        public string Nome { get; private set; }
+        public int VoltasPercorridas { get; private set; } = 0;
+        public int TempoDePitStop { get; private set; }
+
+        public void CompletarVolta()
+        {
+            VoltasPercorridas++;
+            Console.WriteLine($"{Nome} completou uma volta!");
+        }
+
+        public Carro(int tempoPorVolta, string nome, int tempoDePitStop)
+        {
+            TempoDeCadaVolta = tempoPorVolta;
+            Nome = nome;
+            TempoDePitStop = tempoDePitStop;
+        }
+    }
+}
---
+++ Processamento Paralelo/CozinhaMaldita/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace CozinhaMaldita
+{
+    internal class Program
+    {
+        static bool cozinhaAtiva = true;
+        static void Main(string[] args)
+        {
+            Thread cozinheiroPizza = new Thread(() =>
+            {
+                PrepararRefeicao(800, "pizza");
+            });
+            Thread cozinheiroLasanha = new Thread(() =>
+            {
+                PrepararRefeicao(600, "lasanha");
+            });
+            Thread cozinheiroSalada = new Thread(() =>
+            {
+                PrepararRefeicao(400, "salada");
+            });
+
+            cozinheiroPizza.Start();
+            cozinheiroLasanha.Start();
+            cozinheiroSalada.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Coordenando a cozinha...");
+                Thread.Sleep(1000);
+            }
+            cozinhaAtiva = false;
+            Console.WriteLine("Pedidos prontos!");
+        }
+
+        static void PrepararRefeicao(int tempoDePreparo, string nomeDaRefeicao)
+        {
+            while (cozinhaAtiva)
+            {
+                Console.WriteLine($"Preparando {nomeDaRefeicao}...");
+                Thread.Sleep(tempoDePreparo);
+            }
+        }
+    }
+}
