f3e98246fb97a5d32ff7529a8a0e2d6c8c10cc2c
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-04-17 10:47:29 -0300

    atividade importantes

---
+++ Processamento Paralelo/PrioridadeThreads/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace PrioridadeThreads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread audio = new Thread(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    if(i% 1000 == 0)
+                    {
+                        TocarAudio();
+                    }
+                }
+            });
+
+            Thread pathfindingIa = new Thread(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    if (i % 1000 == 0)
+                    {
+                        PathfindingInimigo();
+                    }
+                }
+            });
+
+            audio.Priority = ThreadPriority.Highest;
+            pathfindingIa.Priority = ThreadPriority.Lowest;
+            pathfindingIa.Start();
+            audio.Start();
+            
+            audio.Join();
+            pathfindingIa.Join();
+        }
+
+        static void TocarAudio()
+        {
+            Console.WriteLine("!");
+        }
+
+        static void PathfindingInimigo()
+        {
+            Console.WriteLine(".");
+        }
+    }
+
+    
+}
