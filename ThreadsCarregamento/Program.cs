38671c97b0e883a9e580b1990d4ed14cb37d6c65
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-04-17 10:49:02 -0300

    atividades importantes processamento paralelo

---
+++ ThreadsCarregamento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsCarregamento
+{
+    internal class Program
+    {
+        static bool isLoaded = false;
+        static void Main(string[] args)
+        {
+            Thread carregamentoDeTexturas = new Thread(CarregarTexturas);
+            carregamentoDeTexturas.IsBackground = true;
+            carregamentoDeTexturas.Start();
+            while (isLoaded == false)
+            {
+                Console.WriteLine("Renderizando frame...");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine("Tchauzinho!");
+        }
+
+        static void CarregarTexturas()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Carregando texturas...");
+                Thread.Sleep(500);
+            }
+            isLoaded = true;
+        }
+    }
+}
